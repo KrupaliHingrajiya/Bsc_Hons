8ad05287b48d5de01702850f6f415d7f233dbbd2
KrupaliHingrajiya <krupalidhingrajiya@gmail.com>
2026-07-01 00:06:27 +0530

    Update 11.c

--- semester-3/Data Structure/2./11.c
+++ semester-3/Data Structure/2./11.c
@@ -1,1 +1,37 @@
+#include<stdio.h>
 
+void main()
+{
+    int arr[100],n,i,key,flag=0;
+
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+
+    printf("Enter elements:\n");
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d",&key);
+
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==key)
+        {
+            flag=1;
+            break;
+        }
+    }
+
+    if(flag==1)
+    {
+        printf("Element found at position %d",i+1);
+    }
+    else
+    {
+        printf("Element not found");
+    }
+}
