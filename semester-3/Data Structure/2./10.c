ac1b3dc03b2a8b0e4aec0a244f83bdd68d29bc9f
KrupaliHingrajiya <krupalidhingrajiya@gmail.com>
2026-07-01 00:04:47 +0530

    Update 10.c

--- semester-3/Data Structure/2./10.c
+++ semester-3/Data Structure/2./10.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+int main(){
+  int number,reverse=0,digit;
+  
+  printf("Enter number: ");
+  scanf("%d",&number);
+  
+  while(number>0){
+  digit=number%10;
+  reverse=reverse*10+digit;
+  number=number/10;
+  }
+  
+  printf("Reverse=%d",reverse);
+  
+  return 0;
+}
